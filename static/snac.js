741c663c1229153e4f23b349e57ae21d65f877c2
Sett17 <raik.rohde@gmail.com>
2022-08-21 18:54:19 +0200

    shortcut help modal

--- static/snac.js
+++ static/snac.js
@@ -18,6 +18,17 @@ document.querySelector("#tags span button:nth-child(3)").onclick = () => {
 document.querySelector("#tags span button:nth-child(2)").onclick = () => {
     Misc.updateSidebar()
 }
+document.querySelector("#helpBackground").onclick = () => {
+    document.querySelector("#helpBackground").style.opacity = "0.0"
+    window.setTimeout(() => {
+        document.querySelector("#helpBackground").style.display = 'none'
+    }, 155)
+}
+document.querySelector("#sidebar > div:nth-child(4) > span").onclick = () => {
+    document.querySelector("#helpBackground").style.display = 'flex'
+    document.querySelector("#helpBackground").style.opacity = "1.0"
+}
+
 document.onkeydown = (e) => {
     if (e.key === 's' && e.ctrlKey) {
         Editor.save()
